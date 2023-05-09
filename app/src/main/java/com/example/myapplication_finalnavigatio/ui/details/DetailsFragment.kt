b8614888aa87848d4b38c122cf83fb6db35df053
Admin <impetrov69@gmail.com>
2023-05-09 23:42:17 +0300

    Редактирование неточностй. Часть 9 + ProgressBar

--- app/src/main/java/com/example/myapplication_finalnavigatio/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/myapplication_finalnavigatio/ui/details/DetailsFragment.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication_finalnavigatio.ui.details
 
-import android.graphics.BlurMaskFilter
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,47 +8,34 @@ import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.bumptech.glide.Glide
-import com.example.myapplication_finalnavigatio.R
-import com.example.myapplication_finalnavigatio.catFactApi.FactApi
 import com.example.myapplication_finalnavigatio.databinding.FragmentDetailsBinding
-import com.example.myapplication_finalnavigatio.ui.home.imgURLKey
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 
 class DetailsFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var vm: DetailsViewModel
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        vm = DetailsViewModel()
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         val root = binding.root
 
-        val imgURL = arguments?.getString(imgURLKey)
-        Glide.with(binding.imageViewAnimals)
-            .load(imgURL)
-            .centerCrop()
-            .error(R.drawable.error)
-            .into(binding.imageViewAnimals)
-
-        val retrofit = Retrofit.Builder().baseUrl("https://catfact.ninja/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        addBlur()
-
-        val factApi = retrofit.create(FactApi::class.java)
+        vm.setImg(binding, arguments)
+        vm.addBlur(binding)
         lifecycleScope.launch(Dispatchers.Main) {
-            val fact = withContext(Dispatchers.IO) { factApi.getFactApi().fact }
-            binding.longDescription.text = fact
+            val fact = withContext(Dispatchers.IO) { vm.factApi.getFactApi().fact }
+            if (fact !== null) {
+                binding.progressBar.visibility = View.INVISIBLE
+                binding.longDescription.text = fact
+            }
         }
         return root
     }
@@ -57,12 +43,5 @@ class DetailsFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    fun addBlur(){
-        binding.longDescription.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
-        val radius: Float = binding.longDescription.textSize / 3
-        val filter = BlurMaskFilter(radius, BlurMaskFilter.Blur.SOLID)
-        binding.longDescription.paint.maskFilter = filter
     }
 }
